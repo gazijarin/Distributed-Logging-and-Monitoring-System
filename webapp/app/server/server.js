3a3347d24c91ce206bae9df83a7b47a3aebf7ce7
Gazi <gazijarin@gmail.com>
2021-11-05 19:54:01 -0400

    add react frontend (#20)

--- webapp/app/server/server.js
+++ webapp/app/server/server.js
@@ -1,46 +1,51 @@
-const express = require('express')
-const app = express()
-const config = require('./environment')
-port = config.webapp.port
-const axios = require('axios')
-const { v4: uuidv4 } = require('uuid')
+const express = require("express");
+const app = express();
+const config = require("./environment");
+const PORT = process.env.PORT || 3001;
+const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Distributed Logging and Monitoring System");
+});
 
 // This function gets called by the other log_creator
-app.get('/current_time', (req, res) => {
-  const request_id = req.query['request_id']
+app.get("/current_time", (req, res) => {
+  const request_id = req.query["request_id"];
   let date = new Date();
-  date.toISOString()
-  console.log(request_id, date)
+  date.toISOString();
+  console.log(request_id, date);
   // TODO: send request to logging server
-  res.send(date)
-})
+  res.send(date);
+});
 
-server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+server = app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
 function createLog() {
-  const request_id = uuidv4()
-  console.log(request_id)
-  axios.get(config.webapp.send_to_url + "current_time", {
+  const request_id = uuidv4();
+  console.log(request_id);
+  axios
+    .get(config.webapp.send_to_url + "current_time", {
       params: {
         request_id: request_id
-    }
-  })
-    .then(function (response) {
+      }
+    })
+    .then(function(response) {
       // TODO: send request to logging server
-      console.log("success")
+      console.log("success");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       // TODO: send result to logging server
-      console.log("fail")
-    })
+      console.log("fail");
+    });
 }
 
-const _ = setInterval(createLog, 3000)
+const _ = setInterval(createLog, 3000);
 
-module.exports = {server: server, app: app}
+module.exports = { server: server, app: app };
