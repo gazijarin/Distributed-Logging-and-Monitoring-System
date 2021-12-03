10ce9eca9aab0be75c8ceb9a7ead72ff1058b426
Demetre <demetre.jouras@gmail.com>
2021-12-03 14:36:56 -0500

    Sort logs by timestamp, remove default graphs

--- logging_server/client/src/App.js
+++ logging_server/client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import DataChart from "./DataChart";
 import "./App.css";
-import { DataTable, createData } from "./DataTable";
-import { StyledEngineProvider } from "@mui/material/styles";
+import {createData, DataTable} from "./DataTable";
+import {StyledEngineProvider} from "@mui/material/styles";
 
 function App() {
   const [data, setData] = React.useState([]);
   React.useEffect(() => {
     fetch("/logs/search")
       .then(res => res.json())
-      .then(res1 => {
-        var rows = new Array();
-        res1.map(x => {
-          rows.push(createData(x.id, x.message, x.logLevel, x.timestamp));
+      .then(res => {
+        const rows = [];
+        res.map(x => {
+          rows.unshift(createData(x.id, x.message, x.logLevel, x.timestamp));
+        });
+        rows.sort((a, b) => {
+          return a.timestamp - b.timestamp;
         });
         setData(rows);
       })
@@ -24,9 +26,8 @@ function App() {
       <header className="App-header">
         <h2>{"Distributed Logging and Monitoring System"}</h2>
         <StyledEngineProvider injectFirst>
-          <DataTable rows={data} />
+          <DataTable rows={data}/>
         </StyledEngineProvider>
-        <DataChart chartType="Histogram"></DataChart>
       </header>
     </div>
   );
--- logging_server/server/app.js
+++ logging_server/server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { param } = require('express/lib/router')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 const PrismaClient = require('@prisma/client').PrismaClient
 const prisma = new PrismaClient()
 
